e4363d64ee7955ae6cbb006f10518a2080857874
Samarth Manish Rathod <samarth9578@gmail.com>
2025-10-01 23:58:32 +0530

    Add GFG solution - unique permutation of array POTD- 1 Oct

---
+++ GFG/0005_UniquePermutation_of_array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    vector<vector<int>> uniquePerms(vector<int>& arr) {
+        // code here
+        sort(arr.begin(),arr.end());
+        vector<vector<int>> ans;
+        do{
+            ans.push_back(arr);
+        }while(next_permutation(arr.begin(),arr.end()));
+        return ans;
+        
+    }
+};
